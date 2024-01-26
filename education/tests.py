df9100a8735eac23b27ce134ad95d05c86cfd1de
dmitriyalushkin <pantific37@mail.ru>
2024-01-26 21:06:05 +0300

    Разработка валидаторов, пагинаторов, написание тестов

--- education/tests.py
+++ education/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from education.models import Course, Lesson
+
+
+class CourseTestCase(APITestCase):
+    def setUp(self):
+        self.course = Course.objects.create(
+            name='test',
+            description='test'
+        )
+
+    def test_get_course(self):
+        response = self.client.get('education:course')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class LessonTestCase(APITestCase):
+    def setUp(self):
+        self.lesson = Lesson.objects.create(
+            name='test1',
+            description='test1'
+        )
+
+    def test_get_lesson(self):
+        response = self.client.get('education:lesson-list')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
